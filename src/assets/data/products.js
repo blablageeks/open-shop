77a97bf9c6f12ea0eb8dd6c46b551caeb3f84972
Denis AKPAGNONITE <akpagnonited@gmail.com>
2024-04-13 19:31:58 +0000

    setup store

---
+++ src/assets/data/products.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+        name: "PlayStation 5",
+        description: "Sony playstation 5",
+        sku: "sony-playstation-3458896004",
+        price: 565.99,
+        image: "https://images.unsplash.com/photo-1467949576168-6ce8e2df4e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+        likes: 980,
+        categories: ["electronics", "video-games"],
+    },
+    {
+        name: "Iphone 15",
+        description: "Famous Apple phone",
+        sku: "apple-2345643400-pro-max",
+        price: 995.99,
+        image: "https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+        likes: 87,
+        categories: ["smartphone", "apple", "iphone"],
+    },
+    {
+        name: "Mac Book Pro M3",
+        description: "Famous Apple Mac book pro M3",
+        sku: "2",
+        price: 1999.98,
+        image: "https://images.unsplash.com/photo-1550837368-6594235de85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80",
+        likes: 500,
+        categories: ["apple", "mac-book", "laptop"],
+    },
+    {
+        name: "AWS SAA c03",
+        description: "AWS solutions architect associate certification",
+        sku: "aws-saa-c03-associate",
+        price: 150,
+        image: "https://images.unsplash.com/photo-1551431009-a802eeec77b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=400&q=80",
+        likes: 453,
+        categories: ["cloud", "aws", "certifications"],
+    },
+
+];
+
+window.products = products;
